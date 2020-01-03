2cd175cae3cdcbc56d015c5bf58045157271dace
y-jyothi <43643412+y-jyothi@users.noreply.github.com>
2020-01-03 16:58:04 +0530

    Update firstdemo.tf

--- company/firstdemo.tf
+++ company/firstdemo.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-        access_key= "AKIASEAE2WV6ELDKTAMP"
-        secret_key="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"
+        access_key= "AKIASEAE2WV6AUBFO2BD"
+        secret_key="ECt5Ka/OaRMTnQNvZQGsO2m6+wPbX3xYqx7jbZ3v"
         region="eu-west-3"
 }
-resource "aws_instance" "exampleDemo"{
+resource "aws_instance" "Demo"{
         ami="ami-0f447a1f30868b20d"
         instance_type="t2.micro"
 }
